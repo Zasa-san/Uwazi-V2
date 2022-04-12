26fc6a1843b8a2b3ef59569fab451318c5c6b4f2
Mercy <mercy@huridocs.org>
2022-04-11 20:10:28 -0500

    adds tailwind

--- app/root.tsx
+++ app/root.tsx
@@ -1,14 +1,22 @@
 import * as React from 'react';
-import { Scripts } from '@remix-run/react';
+import { Links, Meta, Scripts } from '@remix-run/react';
+import type { LinksFunction } from "@remix-run/node";
+import tailwindStylesheetUrl from './styles/tailwind.css'
+
+export const links: LinksFunction = () => {
+  return [{ rel: "stylesheet", href: tailwindStylesheetUrl }];
+};
 
 function App() {
   return (
-    <html lang="es">
+    <html lang="es" className="h-full">
       <head>
         <title>Uwazi UI v2</title>
+        <Meta />
+        <Links />
       </head>
-      <body>
-        <h1>Uwazi UI V2</h1>
+      <body className="h-full">
+      <h1 className="text-3xl font-bold underline">Uwazi UI V2</h1>
         <Scripts />
       </body>
     </html>
