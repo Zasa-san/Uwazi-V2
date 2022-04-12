bb6e9644f9d848beb05c1691b001ae58824733a8
Santiago <santiago@huridocs.org>
2022-04-12 13:53:36 -0300

    ts config

--- app/root.tsx
+++ app/root.tsx
@@ -1,11 +1,9 @@
 import * as React from 'react';
 import { Links, Meta, Scripts } from '@remix-run/react';
-import type { LinksFunction } from "@remix-run/node";
-import tailwindStylesheetUrl from './styles/tailwind.css'
+import type { LinksFunction } from '@remix-run/node';
+import tailwindStylesheetUrl from './styles/bundled.css';
 
-export const links: LinksFunction = () => {
-  return [{ rel: "stylesheet", href: tailwindStylesheetUrl }];
-};
+const links: LinksFunction = () => [{ rel: 'stylesheet', href: tailwindStylesheetUrl }];
 
 function App() {
   return (
@@ -16,11 +14,12 @@ function App() {
         <Links />
       </head>
       <body className="h-full">
-      <h1 className="text-3xl font-bold underline">Uwazi UI V2</h1>
+        <h1 className="text-3xl font-bold underline">Uwazi UI V2</h1>
         <Scripts />
       </body>
     </html>
   );
 }
 
+export { links };
 export default App;
